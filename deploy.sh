6c41e06ceb8b4b4f724c54e526add6084c5289d9
A913788 <samuel.hnatiuk@atos.net>
2024-01-16 20:01:57 +0000

    more

---
+++ deploy.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+. .env
+
+config_data=$(jq '.' "$CONFIG_FILE")
+resource_group=$(echo "$config_data" | jq -r '.resource_group')
+workspace_name=$(echo "$config_data" | jq -r '.workspace_name')
+
+endpoint_yaml_content="\$schema: $SCHEMA_URL\nname: $ENDPOINT_NAME\nauth_mode: key"
+echo -e "$endpoint_yaml_content" > endpoint.yaml
+
+deploy_yaml_content=$(cat <<EOF
+\$schema: $SCHEMA_URL
+name: ftse100demodeployment
+endpoint_name: $ENDPOINT_NAME
+model:
+  name: lstm
+  version: 1
+  path: models:/lstm/Staging
+  type: mlflow_model
+instance_type: Standard_DS2_v2
+instance_count: 1
+EOF
+)
+echo "$deploy_yaml_data" > "deployment.yaml"s
+
+# Create endpoint
+az ml online-endpoint create --name "$ep_name" -w "$workspace_name" -g "$resource_group" -f endpoint.yaml
+
+# Create deployment for the endpoint
+az ml online-deployment create -w "$workspace_name" -g "$resource_group" -f deployment.yaml --all-traffic
---
+++ request.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+. .env
+
+config_data=$(jq '.' "$CONFIG_FILE")
+resource_group=$(echo "$config_data" | jq -r '.resource_group')
+workspace_name=$(echo "$config_data" | jq -r '.workspace_name')
+
+secret=$(az ml online-endpoint show-credentials -g $resource_group --n $ep_name -w $workspace_name)
+
+#headers
+content_type="Content-Type: application/json"
+authorization="Authorization: Bearer $secret"
+deployment_header="azureml-model-deployment: $deployment_name"
+
+az ml online-endpoint invoke --name "$ENDPOINT_NAME" --deployment "$deployment_name" --request-file "$input_path" > "$output_path"
