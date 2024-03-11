0e8a42041d5183a5fe00cf963b10cacb1f832286
A913788 <samuel.hnatiuk@atos.net>
2024-03-11 12:37:37 +0000

    small changes

--- scripts/request.sh
+++ scripts/request.sh
@@ -5,7 +5,7 @@ config_data=$(jq '.' "$CONFIG_FILE")
 resource_group=$(echo "$config_data" | jq -r '.resource_group')
 workspace_name=$(echo "$config_data" | jq -r '.workspace_name')
 
-secret=$(az ml online-endpoint show-credentials -g $resource_group --n $ep_name -w $workspace_name)
+secret=$(az ml online-endpoint get-credentials -g $resource_group --n $ep_name -w $workspace_name)
 
 #headers
 content_type="Content-Type: application/json"
